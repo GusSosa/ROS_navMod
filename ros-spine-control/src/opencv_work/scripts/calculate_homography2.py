Homography progrm is functioning - able to calculate length within 1-1.5mm (used standard 8.5x11 in paper for testing). Updated test_H definition to correct for issue with scaling factor, alpha. Updated "global_pts" variable to correct vertical distance between homography dots. Updated misc array data types. Signficant debugging code present, still to be cleaned up

ros-spine-control/src/opencv_work/scripts/calculate_homography2.py
@@ -13,6 +13,9 @@ import cv2
 import rospy
 import imutils
 
+# Set desired frame width in pixels
+PIX_W = 1500
+
 
 def calc_H(uv, xy):
 
@@ -24,10 +27,10 @@ def calc_H(uv, xy):
 
     # Initialize the A and b matrices
     # There will be 2*N rows, since each point has an x, y position
-    A = np.zeros((2 * num_pts, 8), dtype=float)
+    A = np.zeros((2 * num_pts, 8))
     # The b vector will be 2N long
-    b = np.zeros((2 * num_pts, 1), dtype=float)
-    h = np.zeros((8, 1), dtype=float)
+    b = np.zeros((2 * num_pts, 1))
+    h = np.zeros((8, 1))
 
     # print 'A init: ' + str(A)
     # print np.size(A, 0)
@@ -35,30 +38,32 @@ def calc_H(uv, xy):
     # print np.size(b, 0)
 
     # Loop through and build up A and b based on the points matrix.
-    for i in range(0, num_pts):
-        # JACOB MADDEN - 2019_04.15
-        # A[i * 2, :] = np.array(
-        #     [xy[i, 0], xy[i, 1], 1, 0, 0, 0, -uv[i, 0] * xy[i, 0], -uv[i, 0] * xy[i, 1]], dtype=float)
-        # A[i * 2 + 1, :] = np.array(
-        #     [0, 0, 0, xy[i, 0], xy[i, 1], 1, -uv[i, 1] * xy[i, 0], -uv[i, 1] * xy[i, 1]], dtype=float)
+    for i in range(num_pts):
+
         A[i * 2, :] = [xy[i, 0], xy[i, 1], 1, 0, 0, 0, -uv[i, 0] * xy[i, 0], -uv[i, 0] * xy[i, 1]]
         A[i * 2 + 1, :] = [0, 0, 0, xy[i, 0], xy[i, 1], 1, -uv[i, 1] * xy[i, 0], -uv[i, 1] * xy[i, 1]]
         b[i * 2, 0] = uv[i, 0]
         b[i * 2 + 1, 0] = uv[i, 1]
+
+        # # debugging
         # print 'A: ' + str(A)
         # print 'b: ' + str(b)
-        print 'index: ' + str(i)
-        print 'u: ' + str(uv[i, 0])
-        print 'v: ' + str(uv[i, 1])
-        print 'x: ' + str(xy[i, 0])
-        print 'y: ' + str(xy[i, 1])
-        print 'u*x: ' + str(-uv[i, 0] * xy[i, 0])
-        print 'u*y: ' + str(-uv[i, 0] * xy[i, 1])
-        print 'v*x: ' + str(-uv[i, 1] * xy[i, 0])
-        print 'v*y: ' + str(-uv[i, 1] * xy[i, 1])
-        print A[i * 2, :]
-        print A[i * 2 + 1, :]
-        raw_input('break')
+        # print 'index: ' + str(i)
+
+        # print 'u: ' + str(uv[i, 0])
+        # print 'v: ' + str(uv[i, 1])
+        # print 'x: ' + str(xy[i, 0])
+        # print 'y: ' + str(xy[i, 1])
+
+        # print 'u*x: ' + str(-uv[i, 0] * xy[i, 0])
+        # print 'u*y: ' + str(-uv[i, 0] * xy[i, 1])
+        # print 'v*x: ' + str(-uv[i, 1] * xy[i, 0])
+        # print 'v*y: ' + str(-uv[i, 1] * xy[i, 1])
+
+        # print A[i * 2, :]
+        # print A[i * 2 + 1, :]
+
+        # raw_input('break')
 
     # Find homography vector, h, by using least squares minimization
     print 'A: ' + str(np.int64(A))
@@ -101,7 +106,7 @@ def test_H(H, vs):
     # Get the positions of the clicks.
     # We want to wait until the user is ready to start.
     # A nifty way to do so is to have them type something into the terminal, then discard what was typed.
-    raw_input("Please move out of the frame, then press enter to capture the frame that will be used for testing the homography.")
+    raw_input("\n Please move out of the frame, then press enter to capture the frame that will be used for testing the homography.")
     print("Click two times. Distance will be calculated between these two points.")
 
     # Next, get the two clicks. Read this frame:
@@ -109,7 +114,7 @@ def test_H(H, vs):
     # VideoStream or VideoCapture object
     # resize the frame (better viewing, consistent with object tracker.
     frame = vs.read()[1]
-    frame = imutils.resize(frame, width=1750)
+    frame = imutils.resize(frame, width=PIX_W)
 
     # show the newly-captured frame
     cv2.imshow("FrameForTesting", frame)
@@ -145,24 +150,37 @@ def test_H(H, vs):
     # preallocate the xy points
     # it's homogenous, so a 3-vector not a 2-vector
     xy = np.ndarray((3, num_pts))
+    testobj = np.zeros((2, 2))
 
     # plug in for each xy
-    for i in range(0, num_pts):
+    for i in range(num_pts):
         # of the two u, v points that were clicked on, are:
         # this point is (homogenous form)
         uv_i = np.r_[uv[:, i], 1]
-        # then the point in xy is
+        # NOTE: This only solves for [alpha*x, alpha*y, alpha]
         xy[:, i] = np.dot(Q, uv_i)
+        # Divide out scaling factor and solve for [x, y]
+        testobj[:, i] = xy[0:2, i] / xy[2, i]
 
     # Calculate the distance between these two points
     # the net vector between them is
     # this ONLY works with two clicks!!!
     r = xy[:, 0] - xy[:, 1]
+    rtest = testobj[:, 0] - testobj[:, 1]
+    disttest = np.linalg.norm(rtest, 2)
+
+    print 'xy: ' + str(xy)
+    print 'xy_1: ' + str(xy[:, 0])
+    print 'xy_2: ' + str(xy[:, 1])
+    print 'r: ' + str(r)
+    print 'testobj: ' + str(testobj)
+    print 'rtest: ' + str(rtest)
+    print 'disttest: ' + str(disttest)
 
     # the distance is the norm of the net vector
     dist = np.linalg.norm(r, 2)
-    print('Distance between your two points, in cm, is')
-    print(dist)
+    print dist
+    print '\n' + 'Distance between your two points, in cm: ' + str(disttest)
 
     # close the window.
     cv2.destroyWindow("FrameForTesting")

ros-spine-control/src/opencv_work/scripts/opencv_object_tracker.py
@@ -21,7 +21,10 @@ TOT_H_CLICKS = 4
 # Set desired frame width in pixels
 # PIX_W = 1500
 # Drew's laptop screen is 1600x900 so the above is too big.
-PIX_W = 1750
+PIX_W = 1500
+
+# Define number of expected points
+NUM_PT = 8
 
 
 def tracker_init():
@@ -36,11 +39,11 @@ def tracker_init():
     # params.maxThreshold = 200
     # Filter by Area.
     params.filterByArea = True
-    params.minArea = 500  # 1000 previously
+    params.minArea = 500  # NOTE: 750 for 1500W frame; 1500 for 3000W
     # params.maxArea = 2500
     # Filter by Circularity
     params.filterByCircularity = True
-    params.minCircularity = 0.85
+    params.minCircularity = 0.80  # 0.80 reguired for 3000W frame; 0.85 usable for 1500W
     # Filter by Convexity
     params.filterByConvexity = False
     params.minConvexity = 0.95
@@ -197,9 +200,11 @@ def tracker_init():
         # cv2.imshow('Red Keypoints', blob_detection_data['rim_with_keypoints'])
         cv2.imshow('Black Keypoints', blob_detection_data['blim_with_keypoints'])
         # cv2.imshow('Silver Keypoints', blob_detection_data['slim_with_keypoints'])
-        if abs(t2 - t1) > 2:
-            print(blob_detection_data['blcom'])
-            t1 = time.time()
+
+        # debugging: displays the pixel coordinates of the tracked black keypoints
+        # if abs(t2 - t1) > 2:
+        #     print(blob_detection_data['blcom'])
+        #     t1 = time.time()
 
         # initialize the set of information we'll be displaying on
         # the frame
@@ -243,6 +248,16 @@ def tracker_init():
         # auto homography testing
         # C2C distance between left most upper and second upper right holes should be ~56cm
         if key == ord('h'):
+
+            print 'Number of points found for homography: ' + str(len(blob_detection_data['blcom']))
+            if len(blob_detection_data['blcom']) < 8:
+                            # release webcam image
+                vs.release()
+                print('[ERROR: CORRECT NUMBER OF POINTS NOT FOUND]')
+
+                # close all windows and end program
+                cv2.destroyAllWindows()
+                sys.exit()
 
             cv2.destroyAllWindows()
             print('[START OF HOMOGRPAHY TEST]')
@@ -397,7 +412,8 @@ def auto_homography(blob_detection_data):
     # | 0 - |
     # center_dist_vert is the vertical distance between two consecutive points [cm]
     # center_dist_horz is the horizontal distance between the two vertical sets of points [cm]
-    center_dist_vert = 7.62  # 3.0 inches
+    # NOTE: Distance between consecutive points is 7.62/2 (ie 1.5 inches)
+    center_dist_vert = 3.81  # 1.5 inches
     center_dist_horz = 55.88  # 22 inches
     global_pts = np.vstack((np.tile([0, 1], 4) * center_dist_horz, np.array([i * center_dist_vert for i in range(8)]))).transpose()
 